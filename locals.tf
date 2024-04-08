2872c5d12848d692ed641b75f8505d6544cc5c76
priyanshuprafful <priyanshuprafful@gmail.com>
2024-04-08 15:26:29 +0530

    updating ./tf-module-app

--- locals.tf
+++ locals.tf
@@ -2,4 +2,10 @@ locals {
 
   dns_word = var.env == "prod" ? "www" : var.env
   dns_name =  var.component == "frontend" ? "${local.dns_word}.${var.dns_domain}" :"${var.component}-${var.env}.${var.dns_domain}"
+
+  parameters = [var.component , "docdb" , "elasticache"]
+
+
+
 }
+
