442ec1f003ef554f1bcf49cf6de6b5924df9e841
priyanshuprafful <priyanshuprafful@gmail.com>
2024-06-16 05:07:01 +0530

    updating ./tf-module-app

--- main.tf
+++ main.tf
@@ -20,6 +20,15 @@ resource "aws_launch_template" "main" {
     )
 
   }
+
+  tag_specifications {
+    resource_type = "spot-instances-request"
+    tags = merge(
+      var.tags ,
+      { Name = "${var.component}-${var.env}" , Monitor = "yes" }
+    )
+
+  }
   #user_data = filebase64("${path.module}/userdata.sh")
   user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
